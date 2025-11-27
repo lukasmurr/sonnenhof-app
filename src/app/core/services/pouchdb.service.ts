aefc6e4fddace16174f8277f281b5faea678f038
Lukas Murr <120203386+lukasmurr@users.noreply.github.com>
2025-11-27 17:02:24 +0100

    Update pouchdb.service.ts

--- src/app/core/services/pouchdb.service.ts
+++ src/app/core/services/pouchdb.service.ts
@@ -19,7 +19,7 @@ export class CouchDbService {
     private initializeDatabase(): void {
         this.db = new PouchDB('sonnenhof_db');
 
-        this.db.sync(environment.couchdb.remoteUrl, {
+        this.db.sync("http://admin:server-lukas@0.tcp.eu.ngrok.io:18846/sonnenhof_db", {
             live: true,
             retry: true,
             ajax: {
@@ -27,7 +27,7 @@ export class CouchDbService {
                     'ngrok-skip-browser-warning': 'true'
                 },
                 timeout: 30000,
-                withCredentials: true
+                withCredentials: false
             }
         })
             .on('complete', () => {
