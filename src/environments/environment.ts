9e0933493d017a960aad037fbdd4d5c95100f06b
Lukas Murr <120203386+lukasmurr@users.noreply.github.com>
2025-12-08 10:50:59 +0100

    Remove sensitive CouchDB credentials
    Cleared sensitive CouchDB credentials in environment configuration.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     couchdb: {
-        remoteUrl: 'http://195.30.107.67/api/sonnenhof_db',
-        user: 'admin',
-        password: 'gtv-Q+:813Y93P6'
+        remoteUrl: '',
+        user: '',
+        password: ''
     }
 };
